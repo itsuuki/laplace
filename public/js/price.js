e1e9f941aa4f88acaeb61305806b94bff6574851
itsuuki <53384302+itsuuki@users.noreply.github.com>
2020-06-06 16:41:37 +0900

    price-js-2

--- public/js/price.js
+++ public/js/price.js
@@ -94,34 +94,30 @@
 /***/ (function(module, exports) {
 
 $(function () {
-  // console.log($("[class^=remark]"));
   $('.remark').on('keyup', function (e) {
-    // for (let i=0; i<2; i++) {
-    // const l = $("[class^=remark]").attr("class")
-    var k = e.keyCode; // console.log($(this).attr("class"));
-    // console.log($(".remark").eq(i).value);
-    // console.log("remark"+`[${i}]`);
-    // var str = document.getElementById("remark").value;
-    // console.log($(this).val());
+    var str = $(this).val();
 
-    var str = $(this).val(); // const str = document.getElementsByClassName(l).value;
-    // var str = $(".remark").eq(i).html();
-    // });
-
-    if (!(str.match(/[0-9]/) || 37 <= k && k <= 40 || k === 8 || k === 46)) {
+    if (str.match(/[^0-9.,-]+/)) {
+      alert("半角数字で入力して下さい。");
       return false;
     } else {
-      // コンソールに成人と表示
-      // console.log(document.getElementById("com-price").dataset.price);
-      console.log(str);
-    } // }
+      var kk = $(this).parent();
+      var item_price = kk.children().data("price");
+      var mm = kk.find('.com-price');
+      mm.html(item_price * str);
+      var total = 0;
+      var array = [];
+      var nn = document.getElementsByClassName("com-price");
 
-  });
-  $('.remark').on('keyup', function (e) {
-    this.value = this.value.replace(/[^0-9]+/i, '');
-  });
-  $('.remark').on('blur', function () {
-    this.value = this.value.replace(/[^0-9]+/i, '');
+      for (i = 0; i < nn.length; i++) {
+        var totals = nn[i].innerHTML;
+        var total = total + Number(totals);
+        array.push(total);
+        var total_price = array.pop();
+      }
+
+      document.getElementById("total-price").innerHTML = total_price;
+    }
   });
 });
 
